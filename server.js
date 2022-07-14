1c6aae6933ccb64474973f99f86891f9f368f87d
yang <joey674901205@gmail.com>
2022-07-14 16:38:26 -0400

    add express.js, PORT, and heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const { notes } = require('./db/db');
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    console.log(req.query)
+    res.json(results);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
